90a9cbabc51382f7431f9239d44ac9aef5870a46
lmr3796 <lmr3796joseph@yahoo.com.tw>
2011-12-10 15:59:01 +0800

    Main code of the server.

---
+++ src/server.py
@@ -0,0 +1,21 @@
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+class MainPage(webapp.RequestHandler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/plain'
+        self.response.out.write('Hello, webapp World!')
+
+
+url = [
+        ('.+', MainPage),
+        ]
+application = webapp.WSGIApplication(
+        url,
+        debug=True)
+
+def main():
+    run_wsgi_app(application)
+
+if __name__ == "__main__":
+    main()

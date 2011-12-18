62efea4d836f532aa73423c1244ccfbd7a76cb41
lmr3796 <lmr3796joseph@yahoo.com.tw>
2011-12-18 23:47:54 +0800

    kerker

--- src/facebookMiner.py
+++ src/facebookMiner.py
@@ -9,28 +9,29 @@ from httplib import HTTPSConnection
 from urllib import urlencode
 
 class FacebookMiner(object):
-	def __init__(self,access_token):
-		self.graph = facebook.GraphAPI(access_token);
-		return
+    def __init__(self,access_token):
+        self.graph = facebook.GraphAPI(access_token);
+        return
 
-	def find_places(self,lat,lng,distance=50):
-		arg = r'SELECT page_id,latitude,longitude FROM place WHERE distance(latitude,longitude,"'+lat+'","'+lng+'")<'+str(distance)
-		args = {'q':arg}
-		response = self.graph.request('fql',args)
-		places = []
-		for place in response['data']:
-			result = {}
-			result['pid'] = place['page_id']
-			result['lat'] = place['latitude']
-			result['long'] = place['longitude']
-			places.append(result)
-		return places
+    def find_places(self,lat,lng,distance=200):
+        arg = r'SELECT page_id, latitude, longitude, name FROM place WHERE distance(latitude,longitude,"'+lat+'","'+lng+'")<'+str(distance)
+        args = {'q':arg}
+        response = self.graph.request('fql',args)
+        places = []
+        for place in response['data']:
+            result = {}
+            result['pid'] = place['page_id']
+            result['lat'] = place['latitude']
+            result['long'] = place['longitude']
+            result['name'] = place['name']
+            places.append(result)
+        return places
 
-	def load_checkins(self, checkin_id):
-		message = []
-		response = self.graph.get_object(checkin_id+'/checkins')
-		for checkin in response['data']:
-			if checkin.has_key('message') == True:
-				message.append(checkin['message'])
-		return message
-	
+    def load_checkins(self, checkin_id):
+        message = []
+        response = self.graph.get_object(checkin_id+'/checkins')
+        for checkin in response['data']:
+            if checkin.has_key('message') == True:
+                message.append(checkin['message'])
+        return message
+
--- src/server.py
+++ src/server.py
@@ -1,13 +1,15 @@
 import logging
 import urllib
 from httplib import HTTPConnection
+from google.appengine.api import urlfetch
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from django.utils import simplejson as json
 from facebook import GraphAPIError
 from facebookMiner import FacebookMiner 
 
-YAHOO_APP_ID = r'Tsvd7E3V34FE2Iva.bjSC0gdXsY.3KA4KMR3sRpHiradFSNyZ5wonLrX79u38NocIQCGqA--'
+YAHOO_APP_ID1 = r'Tsvd7E3V34FE2Iva.bjSC0gdXsY.3KA4KMR3sRpHiradFSNyZ5wonLrX79u38NocIQCGqA--'
+YAHOO_APP_ID2 = r'Gv5SAfHV34F7pX6ttIe8G9.2EeHW50BjkGSHX7LO9geKHMBYALrJ9L4ujjsdDvDRAeEaeQ--'
 
 class MainPage(webapp.RequestHandler):
     def get(self):
@@ -16,46 +18,63 @@ class MainPage(webapp.RequestHandler):
 
 class YahooAPI:
     def __init__(self, app_id=None, th=None):
-        YAHOO='asia.search.yahooapis.com'
-        self.conn=HTTPConnection(YAHOO)
+        self.server='asia.search.yahooapis.com'
+        #self.conn=HTTPConnection(YAHOO)
         self.data = {
             'format'    :'json',
             'appid'     :app_id,
             'content'   :'',
-            'threshold'  :100,
+            'threshold' :0,
         }
 
     def split(self, text):
-        YAHOO_SPLIT_PATH='/cas/v1/ke'
+        SPLIT_PATH='/cas/v1/ke'
         self.data['content'] = text.encode('utf-8')
-        self.conn.connect()
-        self.conn.request('POST', YAHOO_SPLIT_PATH, urllib.urlencode(self.data), {'Content-type':'application/x-www-form-urlencoded'})
-        response = self.conn.getresponse()
-        words = json.loads(response.read())
-        result = []
-        if response.status == 200:
+        response = urlfetch.fetch('http://'+self.server+SPLIT_PATH, urllib.urlencode(self.data), 
+                                    urlfetch.POST,{'Content-type':'application/x-www-form-urlencoded'})
+        #self.conn.connect()
+        #self.conn.request('POST', YAHOO_SPLIT_PATH, urllib.urlencode(self.data), {'Content-type':'application/x-www-form-urlencoded'})
+        #response = self.conn.getresponse()
+        if response.status_code == 200:
+            if response.content_was_truncated:
+                raise NameError('Content was truncated')
+            words = response.content
+            words = json.loads(words)
+            result = [] 
             if words is not None:
-                #for w in words:
-                #    result.append((w['token'], w['score']))
-                result=[(w['token'], w['score']) for w in words]
+                result = [(w['token'], w['score']) for w in words]
         else:
             raise NameError(str(response.status) + ' ' + response.reason)
-        self.conn.close()
+        #self.conn.close()
         return result
 
 class FB(webapp.RequestHandler):
-    
+    def key_word_group(self, kw_list):
+        limit = (4, 8, 16)
+        result = [[],[],[]]
+        for kw in kw_list:
+            for i in range(0, len(limit)):
+                if len(result[i]) < limit[i]:
+                    result[i].append(kw)
+                    break
+        return result
+
     def cloud_maker(self, lat, lng):
-        global YAHOO_APP_ID
-        yahoo = YahooAPI(YAHOO_APP_ID)
+        global YAHOO_APP_ID2
+        yahoo = YahooAPI(YAHOO_APP_ID2)
         tag_cloud=[]
         places = self.facebook.find_places(lat, lng)
-        for pid in places:
-            checkins = self.facebook.load_checkins(str(pid))
+        for p in places:
+            checkins = self.facebook.load_checkins(str(p['pid']))
             if not checkins:
                 continue
-            place = sum([yahoo.split(ckin) for ckin in checkins], [])
-            tag_cloud.append()
+            all_checkin = '\n'.join([ckin for ckin in checkins])
+            #logging.info(unicode(all_checkin).encode('utf-8'))
+            kw = yahoo.split(all_checkin)
+            kw = sorted(kw, key=lambda keyword: keyword[1], reverse=True)
+            kw = self.key_word_group([k[0] for k in kw])
+            tag_cloud.append({'id':str(p['pid']), 'name':p['name'],
+                                'lat': p['lat'], 'lng': p['long'], 'kw':kw})
         return tag_cloud
     
     def get(self):
@@ -67,20 +86,18 @@ class FB(webapp.RequestHandler):
             lat = self.request.get('lat')
         except (TypeError, ValueError):
             result['message'] ='Bad input'
-        except Exception as (errno, strerror):
+        except Exception as e:
             result['message'] = 'Unknown Error' 
-            logging.error('Some uncaptured exceptions thrown')
-            raise
+            logging.error(str(e))
         else:
             self.facebook = FacebookMiner(token) 
             try:
                 tag_cloud = self.cloud_maker(lat, lng)
             except GraphAPIError:
                 result['message'] = 'Facebook Graph API Error'
-            except:
+            except Exception as e:
                 result['message'] = 'Unknown Error' 
-                logging.error('Some uncaptured exceptions thrown')
-                raise
+                logging.error(str(e))
             else:
                 result['result'] = tag_cloud
                 result['status'] = '0'
